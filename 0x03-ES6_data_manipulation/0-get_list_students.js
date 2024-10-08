f70a2dd5f14ebddfe3d2e84f9c6139c403312409
Akram-Adam <akramadam050@gmail.com>
2024-10-08 23:21:05 +0000

    ES6

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,13 @@
-export default function getListStudents() {
-  return [
+const getListStudents = () => {
+  const array = [ 
+         
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-}
+    return array;
 
+  };
+  
+
+export default getListStudents;
